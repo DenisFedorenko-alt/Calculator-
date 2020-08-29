626dcff41eeabfe60c120a41cd4db7f4136a084b
Денис Федоренко <deniskasosiska72@gmail.com>
2020-08-29 16:08:29 +0300

    version 1.0

---
+++ app.js
@@ -0,0 +1,33 @@
+function insert(num) {
+    sqreen.innerHTML = document.querySelector(".sqreen").innerHTML + num;
+}
+
+function cleanSqreen() {
+    sqreen.innerHTML = ""
+}
+
+function removeLastNumber() {
+    let newText = sqreen.textContent.substr(0, sqreen.textContent.length - 1);
+    sqreen.innerHTML = newText
+}
+
+function equalNumbs() {
+    if (sqreen.textContent) {
+        sqreen.textContent = eval(sqreen.textContent)
+    } else {
+        console.log("dddd")
+    }
+}
+
+document.querySelectorAll(".button").forEach(button => button.addEventListener("click", (e) => {
+    insert(e.target.textContent)
+}))
+
+const clean = document.querySelector(".clean");
+const deleteNum = document.querySelector(".delete");
+let sqreen = document.querySelector(".sqreen");
+const equal = document.querySelector(".equal");
+
+equal.addEventListener("click", equalNumbs)
+clean.addEventListener("click", cleanSqreen);
+deleteNum.addEventListener("click", removeLastNumber);
